e849e37c7e8bab3994c75b9fdd1d660f6fbc191d
tacoteambeyblade <tacoteambeyblade@gmail.com>
2026-06-25 20:43:59 -0600

    Added UI logic to sort and print the elements

---
+++ scores/main.js
@@ -0,0 +1,41 @@
+import bladers from "../data/bladers.json" with { type: "json"}
+
+function sortRanking(){
+    // ... spread operator to not modify the original values
+    // uses - due sort only uses positive and negative values not booleans
+    const ranking = [...bladers].sort((a,b) => b.score - a.score)
+
+    const container = document.getElementById("ranking");
+
+    ranking.forEach((blader, index) =>{
+        console.log(`${index+1}. ${blader.names[0]} -> ${blader.score}`)
+    })
+
+    let shared_ranking = []
+    let curent = 1
+    let past = null 
+
+    ranking.forEach((blader, index) => {
+        if(blader.score !== past){
+            curent = index+1
+            past = blader.score
+        }
+
+        shared_ranking.push({curent, ...blader})
+
+        // Creates the dom elements
+        const item = document.createElement("div");
+        item.classList.add("blader");
+
+        item.innerHTML = `
+            <strong>${curent}. ${blader.names[0].toUpperCase()}</strong>
+            <span>${blader.score} pts</span>
+        `;
+
+        container.appendChild(item);
+    })
+
+    
+}
+
+sortRanking()
